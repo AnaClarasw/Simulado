26032fc2097828d6e9509472b3d34873bf2124b7
anaclara <anaclara.sw@hotmail.com>
2021-09-23 17:13:19 -0300

    foiii o routess

--- react/site/src/index.js
+++ react/site/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './routes';
 import reportWebVitals from './reportWebVitals';
-import Routes from './routes';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +12,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
