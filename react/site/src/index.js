63e874d4642ec842c5fd8b08bca21cde3babe35b
anaclara <anaclara.sw@hotmail.com>
2021-09-23 17:08:42 -0300

    deu certo

--- react/site/src/index.js
+++ react/site/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Routes from './routes';
 import reportWebVitals from './reportWebVitals';
+import Routes from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
