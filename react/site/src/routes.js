18076e9962b750c77ed128cb3bdc8e488f60028a
anaclara <anaclara.sw@hotmail.com>
2021-09-23 17:31:56 -0300

    ok

--- react/site/src/routes.js
+++ react/site/src/routes.js
@@ -1,14 +1,13 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Alunos from './pages/alunos';
-import Error from './pages/error';
+
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact={true} component={Alunos} />
-                <Route path="*" component={Error} />
             </Switch>
         </BrowserRouter>
     )
